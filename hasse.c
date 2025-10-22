530e39b00f978c023164293f8a755c9c3eb4d3ef
D0mano <njfotso@gmail.com>
2025-10-22 10:53:52 +0200

    Add definition for base function and types

--- hasse.c
+++ hasse.c
@@ -1,5 +1,60 @@
 #include <malloc.h>
 #include "hasse.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "types.h"
+
+t_cell* createCell(int value) {
+    t_cell* cell = (t_cell*) malloc(sizeof(t_cell));
+    cell->value = value;
+    cell->next = NULL;
+    cell->arrival = NULL;
+    return cell;
+}
+t_list createEmptyList() {
+    t_list list;
+    list.head = NULL;
+    return list;
+}
+int isEmptyList(t_list list) {
+    return list.head == NULL;
+}
+void addCell(t_list* list,int val) {
+    t_cell* new_cell = createCell(val);
+    if (isEmptyList(*list)) {
+        list->head = new_cell;
+    }else {
+        t_cell* cur = list->head;
+        while (cur->next != NULL) {
+            cur = cur->next;
+        }
+        cur->next = new_cell;
+    }
+}
+void displayList(t_list list){
+    t_cell* cur = list.head;
+    printf("[head|@]->");
+    while (cur != NULL) {
+        printf("[%d|@]->", cur->value);
+        cur = cur->next;
+    }
+    printf("NULL\n");
+}
+t_adjacency_list* createAdjList(int nb_vertices) {
+    t_adjacency_list* adjacency_list = (t_adjacency_list*) malloc(sizeof(t_adjacency_list));
+    adjacency_list->size = nb_vertices;
+    adjacency_list->tab = (t_list*) malloc(adjacency_list->size * sizeof(t_list));
+    for (int i = 0; i < adjacency_list->size; i++) {
+        adjacency_list->tab[i] = createEmptyList();
+    }
+    return adjacency_list;
+}
+void displayAdjList(t_adjacency_list* adj) {
+    for (int i = 0; i < adj->size; i++) {
+        printf("List for vertex %d:",i);
+        displayList(adj->tab[i]);
+    }
+}
 
 
 void removeTransitiveLinks(t_link_array *p_link_array)
--- hasse.h
+++ hasse.h
@@ -1,5 +1,65 @@
 #ifndef __HASSE_H__
 #define __HASSE_H__
+#include "types.h"
+
+
+typedef struct s_cell{
+    struct s_cell* arrival;
+    struct s_cell* next;
+    int value;
+}t_cell;
+
+typedef struct s_list {
+    t_cell* head;
+}t_list;
+
+typedef struct s_adjacency_list {
+    int size;
+    t_list* tab;
+
+}t_adjacency_list;
+
+/**
+ * @brief Create a cell with tha value
+ * @param value The value in the cell
+ * @return t_cell*  A pointers to a t_cell
+ */
+t_cell* createCell(int value);
+
+/**
+ * @brief Create an Empty linked list
+ * @return The empty list
+ */
+t_list createEmptyList();
+
+/**
+ *
+ * @param list The list who will be verified
+ * @return
+ */
+int isEmptyList(t_list list);
+
+/**
+ * @brief Add a cell with the value in the list
+ * @param list The list where the value will be added
+ * @param val The value to be added to the list
+ */
+void addCell(t_list* list,int val);
+
+/**
+ *
+ * @param list The list to be displayed
+ */
+void displayList(t_list list);
+
+/**
+ *
+ * @param nb_vertices The size of your adjacency list
+ * @return
+ */
+t_adjacency_list* createAdjList(int nb_vertices);
+
+void displayAdjList(t_adjacency_list* adj);
 
 
 void removeTransitiveLinks(t_link_array *p_link_array);
