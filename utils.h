14fa85b1fcda64d979819b0f1d68b0c919d7de17
benoitTailhades <benoit.ths@gmail.com>
2025-11-21 08:17:07 +0100

    main clarifications

--- utils.h
+++ utils.h
@@ -1,13 +1,19 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
-
 /**
- * @brief Converte an interger into the associated string
+ * @brief Convert an integer into the associated string
  * @param num The integer to be converted
  * @return The associated string
  */
 char *getID(int num);
+
+/**
+ * @brief Calculate the minimum of two integers
+ * @param a The first integer
+ * @param b The second integer
+ * @return The smaller value between a and b
+ */
 int min(int a, int b);
 
 #endif
